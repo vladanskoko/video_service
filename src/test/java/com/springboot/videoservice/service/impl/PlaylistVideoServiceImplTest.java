4d26fec14f0ffbf182a0918a3b67b398ddf17ba2
Vladan Skoko <vladan.skoko@axiomq.com>
2022-04-26 13:13:52 +0200

    Create unit tests for remove and sort videos in playlist

--- src/test/java/com/springboot/videoservice/service/impl/PlaylistVideoServiceImplTest.java
+++ src/test/java/com/springboot/videoservice/service/impl/PlaylistVideoServiceImplTest.java
@@ -10,7 +10,10 @@ import com.springboot.videoservice.service.PlaylistVideoService;
 import com.springboot.videoservice.service.VideoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
@@ -22,25 +25,38 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class PlaylistVideoServiceImplTest {
     private PlaylistVideoService playlistVideoService;
-    private PlaylistService playlistServiceMock;
-    private VideoService videoServiceMock;
-    private PlaylistVideoRepository playlistVideoRepositoryMock;
-    private Video video;
-    private Playlist playlist;
 
+    @Mock
+    private PlaylistService playlistServiceMock;
+
+    @Mock
+    private VideoService videoServiceMock;
+
+    @Mock
+    private PlaylistVideoRepository playlistVideoRepositoryMock;
+
+    private Video video1;
+    private Video video2;
+    private Video video3;
+
+    private Playlist playlist;
 
     @BeforeEach
     void setUp() {
-        playlistServiceMock = mock(PlaylistService.class);
-        playlistVideoRepositoryMock = mock(PlaylistVideoRepository.class);
-        videoServiceMock = mock(VideoService.class);
         playlistVideoService = new PlaylistVideoServiceImpl(playlistVideoRepositoryMock, playlistServiceMock, videoServiceMock);
 
-        video = new Video();
-        video.setId(1L);
-        video.setName("video1");
+        video1 = new Video();
+        video1.setId(1L);
+        video1.setName("video1");
+        video2 = new Video();
+        video2.setId(2L);
+        video2.setName("video2");
+        video3 = new Video();
+        video3.setId(3L);
+        video3.setName("video3");
         playlist = new Playlist();
         playlist.setId(1L);
         playlist.setName("playlist1");
@@ -50,11 +66,11 @@ class PlaylistVideoServiceImplTest {
     void givenEmptyPlaylistAndVideo_whenAddVideoToPlaylist_thenReturnPlaylistVideoListWithAddedVideoAtBeggining() {
         //given
         List<PlaylistVideo> testPVList = new ArrayList<>();
-        PlaylistVideo playlistVideo = new PlaylistVideo(playlist, video, 1);
+        PlaylistVideo playlistVideo = new PlaylistVideo(playlist, video1, 1);
         PlaylistVideo playlistVideoFromDB = new PlaylistVideo();
 
         when(playlistServiceMock.getPlaylistById(eq(1L))).thenReturn(playlist);
-        when(videoServiceMock.getVideoById(eq(1L))).thenReturn(video);
+        when(videoServiceMock.getVideoById(eq(1L))).thenReturn(video1);
         when(playlistVideoRepositoryMock.getPlaylistVideosByPlaylist(eq(playlist))).thenReturn(testPVList);
         when(playlistVideoRepositoryMock.save(eq(playlistVideo))).thenReturn(playlistVideo);
 
@@ -62,7 +78,7 @@ class PlaylistVideoServiceImplTest {
         PlaylistVideo actualPlaylistVideo = playlistVideoService.addVideoToPlaylist(1L, 1L);
 
         //then
-        verifyAll();
+        verifyAll(1, 1, 1, 1, 0);
         assertEquals(playlistVideo, actualPlaylistVideo);
     }
 
@@ -70,10 +86,10 @@ class PlaylistVideoServiceImplTest {
     void givenPlaylistAndVideo_whenAddVideoToPlaylist_thenReturnPlaylistVideoListWithAddedVideoToTheEnd() {
         //given
         List<PlaylistVideo> testPVList = new ArrayList<>(List.of(new PlaylistVideo()));
-        PlaylistVideo playlistVideo = new PlaylistVideo(playlist, video, 2);
+        PlaylistVideo playlistVideo = new PlaylistVideo(playlist, video1, 2);
 
         when(playlistServiceMock.getPlaylistById(eq(1L))).thenReturn(playlist);
-        when(videoServiceMock.getVideoById(eq(1L))).thenReturn(video);
+        when(videoServiceMock.getVideoById(eq(1L))).thenReturn(video1);
         when(playlistVideoRepositoryMock.getPlaylistVideosByPlaylist(eq(playlist))).thenReturn(testPVList);
         when(playlistVideoRepositoryMock.save(eq(playlistVideo))).thenReturn(playlistVideo);
 
@@ -81,7 +97,7 @@ class PlaylistVideoServiceImplTest {
         PlaylistVideo actualPlaylistVideo = playlistVideoService.addVideoToPlaylist(1L, 1L);
 
         //then
-        verifyAll();
+        verifyAll(1, 1, 1, 1, 0);
         assertEquals(playlistVideo, actualPlaylistVideo);
     }
 
@@ -94,6 +110,7 @@ class PlaylistVideoServiceImplTest {
         Executable executable = () -> playlistVideoService.addVideoToPlaylist(222L, 1L);
 
         //then
+        verifyAll(0, 0, 0, 0, 0);
         assertThrows(ResourceNotFoundException.class, executable);
     }
 
@@ -106,13 +123,105 @@ class PlaylistVideoServiceImplTest {
         Executable executable = () -> playlistVideoService.addVideoToPlaylist(1L, 222L);
 
         //then
+        verifyAll(0, 0, 0, 0, 0);
         assertThrows(ResourceNotFoundException.class, executable);
     }
 
-    private void verifyAll() {
-        verify(playlistServiceMock, times(1)).getPlaylistById(anyLong());
-        verify(videoServiceMock, times(1)).getVideoById(anyLong());
-        verify(playlistVideoRepositoryMock, times(1)).getPlaylistVideosByPlaylist(any());
-        verify(playlistVideoRepositoryMock, times(1)).save(any());
+    @Test
+    void givenPlaylistAndVideo_whenDeleteVideoFromPlaylist_thenVideoIsRemovedFromPlaylist() {
+        //given
+        List<PlaylistVideo> testPVList = new ArrayList<>();
+        PlaylistVideo testPV = new PlaylistVideo(playlist, video1, 1);
+        testPVList.add(testPV);
+
+        when(playlistServiceMock.getPlaylistById(eq(1L))).thenReturn(playlist);
+        when(videoServiceMock.getVideoById(eq(1L))).thenReturn(video1);
+        when(playlistVideoRepositoryMock.getPlaylistVideosByPlaylist(eq(playlist))).thenReturn(testPVList);
+        when(playlistVideoRepositoryMock.getPlaylistVideoByPlaylistAndVideo(eq(playlist), eq(video1))).thenReturn(testPV);
+
+        doAnswer(invocation -> {
+            PlaylistVideo arg0 = invocation.getArgument(0);
+
+            //then
+            verifyAll(1, 1, 1, 0, 1);
+            assertEquals("video1", arg0.getVideo().getName());
+            return arg0;
+        }).when(playlistVideoRepositoryMock).delete(any(PlaylistVideo.class));
+
+        //when
+        playlistVideoService.removeVideoFromPlaylist(1L, 1L);
+    }
+
+    @Test
+    void givenUnexistentPlaylistAndVideo_whenDeleteVideoFromUnexistentPlaylist_thenThrowException() {
+        //given
+        when(playlistServiceMock.getPlaylistById(eq(222L))).thenThrow(ResourceNotFoundException.class);
+
+        //when
+        Executable executable = () -> playlistVideoService.removeVideoFromPlaylist(222L,1L);
+
+        //then
+        verifyAll(0, 0, 0, 0, 0);
+        assertThrows(ResourceNotFoundException.class, executable);
+    }
+
+    @Test
+    void givenUnexistentVideoAndPlaylist_whenDeleteUnexistentVideoFromPlaylist_thenThrowException() {
+        //given
+        when(videoServiceMock.getVideoById(eq(222L))).thenThrow(ResourceNotFoundException.class);
+
+        //when
+        Executable executable = () -> playlistVideoService.removeVideoFromPlaylist(1L, 222L);
+
+        //then
+        verifyAll(0, 0, 0, 0, 0);
+        assertThrows(ResourceNotFoundException.class, executable);
+    }
+
+    @Test
+    void givenPlaylistWithVideos_whenSortVideosInPlaylist_thenReturnSortedPlaylistByOrderNumber() {
+        //given
+        PlaylistVideo playlistVideo1 = new PlaylistVideo(playlist, video1, 3);
+        PlaylistVideo playlistVideo2 = new PlaylistVideo(playlist, video2, 1);
+        PlaylistVideo playlistVideo3 = new PlaylistVideo(playlist, video3, 2);
+        List<PlaylistVideo> testPVList = new ArrayList<>(List.of(playlistVideo1, playlistVideo2, playlistVideo3));
+        List<PlaylistVideo> actualPVList;
+
+        when(playlistServiceMock.getPlaylistById(eq(1L))).thenReturn(playlist);
+        when(playlistVideoRepositoryMock.getPlaylistVideosByPlaylist(eq(playlist))).thenReturn(testPVList);
+
+        //when
+        actualPVList = playlistVideoService.sortPlaylistVideos(1L);
+
+        //then
+        verifyAll(1, 0, 1, 0, 0);
+        assertEquals("video2", actualPVList.get(0).getVideo().getName());
+        assertEquals("video3", actualPVList.get(1).getVideo().getName());
+        assertEquals("video1", actualPVList.get(2).getVideo().getName());
+    }
+
+    @Test
+    void givenUnexistentPlaylist_whenSortVideosInUnexistentPlaylist_thenThrowException() {
+        //given
+        when(playlistServiceMock.getPlaylistById(eq(222L))).thenThrow(ResourceNotFoundException.class);
+
+        //when
+        Executable executable = () -> playlistVideoService.sortPlaylistVideos(222L);
+
+        //then
+        assertThrows(ResourceNotFoundException.class, executable);
+    }
+
+    private void verifyAll(int playlistServiceMockNum,
+                           int videoServiceMockNum,
+                           int playlistVideoRepositoryMockNum,
+                           int playlistVideoRepositoryMockSaveNum,
+                           int playlistVideoRepositoryMockDeleteNum) {
+
+        verify(playlistServiceMock, times(playlistServiceMockNum)).getPlaylistById(anyLong());
+        verify(videoServiceMock, times(videoServiceMockNum)).getVideoById(anyLong());
+        verify(playlistVideoRepositoryMock, times(playlistVideoRepositoryMockNum)).getPlaylistVideosByPlaylist(any());
+        verify(playlistVideoRepositoryMock, times(playlistVideoRepositoryMockSaveNum)).save(any());
+        verify(playlistVideoRepositoryMock, times(playlistVideoRepositoryMockDeleteNum)).delete(any());
     }
 }
