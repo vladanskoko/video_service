f8b56fa14a36f0a88cb2b1a3eae79dd209c9f2b8
Vladan Skoko <vladan.skoko@axiomq.com>
2022-05-04 13:19:51 +0200

    Create Basic auth security

---
+++ src/main/java/com/springboot/videoservice/security/ApplicationSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.springboot.videoservice.security;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(@NotNull HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails skole = User.builder()
+                .username("skole")
+                .password(passwordEncoder.encode("pass1"))
+                .roles("student")
+                .build();
+        return new InMemoryUserDetailsManager(skole);
+    }
+}
